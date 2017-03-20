6d78afc463f2ebb31b808cbf40b0d669f6e89d7f
HoldenWells <bholden.wells@gmail.com>
2017-03-20 12:55:40 -0500

    Holden Changes
    added textboxes and different animations

--- classes/player.lua
+++ classes/player.lua
@@ -1,7 +1,56 @@
-local class = require("lib.middlecalss")
-local entity = require("classes.entity")
+local class = require("lib.middleclass")
 
-local player = class("player", entity)
 
+local player = class("player")
+
+local spritesheet = love.graphics.newImage("Sprites/PlayerSprites.png")
+local walkdown = newAnimation(spritesheet, 64, 64, 0.1, 1, 4)
+local walkup = newAnimation(spritesheet, 64, 64, 0.1, 13, 16)
+local walkleft = newAnimation(spritesheet, 64, 64, 0.1, 5, 8)
+local walkright = newAnimation(spritesheet, 64, 64, 0.1, 9, 12)
+walkdown:setSpeed(0.5)
+walkup:setSpeed(0.5)
+walkleft:setSpeed(0.5)
+walkright:setSpeed(0.5)
+
+function player:initialize()
+    self.x=0
+    self.y=0
+    self.h=30
+    self.w=30
+    self.speed=2
+end
+
+function player:update(dt)
+    walkright:update(dt)
+    walkleft:update(dt)
+    walkup:update(dt)
+    walkdown:update(dt)
+    if(love.keyboard.isDown("d")) then
+        self.x=self.x+self.speed*dt
+    end
+    if(love.keyboard.isDown("a")) then
+        self.x=self.x-self.speed*dt
+    end
+    if(love.keyboard.isDown("w")) then
+        self.y=self.y-self.speed*dt
+    end
+    if(love.keyboard.isDown("s")) then
+        self.y=self.y+self.speed*dt
+    end
+  end
+
+function player:draw()
+    if(love.keyboard.isDown("d")) then
+        walkright:draw(self.x, self.y)
+    elseif(love.keyboard.isDown("w")) then
+        walkup:draw(self.x, self.y)
+    end
+    if(love.keyboard.isDown("a")) then
+        walkleft:draw(self.x, self.y)
+    elseif(love.keyboard.isDown("s")) then
+        walkdown:draw(self.x, self.y)
+    end
+end
 
 return player
--- main.lua
+++ main.lua
@@ -1,15 +1,33 @@
-local lovebird = require("lib.lovebird")
+require("lib.textbox")
+require("lib.AnAL")
+
+local gamera = require "lib.gamera"
 local map = require("classes.map")
+local p = require("classes.player")
+local tb = require("classes.textbox")
 
 function love.load()
-    test = map("maps.animation")
+  text = {
+      "Hello Adventurer", "I used to be an adventurer like you", "until I took an arrow to the knee"
+  }
+  test = map("Maps.Testmap")
+  player=p()
+  textbox = tb(text)
+  cam = gamera.new(0,0,512,512)
+  cam:setScale(1.5)
+  cam:setPosition(player.x, player.y)
 end
 
-function love.update()
-    lovebird:update()
-
+function love.update(dt)
+  cam:setPosition(player.x, player.y)
+  player:update(dt)
+  textbox:update(dt)
 end
 
 function love.draw()
-    test:draw()
+  cam:draw(function(l,t,w,h)
+      test:draw(0,0)
+    player:draw()
+  end)
+  textbox:draw()
 end
