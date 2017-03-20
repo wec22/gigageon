3f6960f43421372ce5451c847c7d34492eb8304a
HoldenWells <bholden.wells@gmail.com>
2017-03-20 14:55:45 -0500

    fixed!
    everything works

--- classes/textbox.lua
+++ classes/textbox.lua
@@ -7,32 +7,35 @@ local arrow = love.graphics.newImage("Sprites/textarrow.png")
 local textarrow = newAnimation(arrow, 16, 16, 0.1, 1, 2)
 textarrow:setSpeed(0.1)
 
-function textbox:initialize(text, cooldown)
+function textbox:initialize(text)
     self.text = text
-    self.cooldown = cooldown
+    self.cooldown = 0
     self.index = 1
 end
 
 function textbox:update(dt)
+    textarrow:update(dt)
     if self.cooldown ~= 0 then
         self.cooldown = self.cooldown-1
     end
 end
 
 function textbox:draw()
-    --if(text[index]) then
+    if(self.text[self.index]) then
         local textboxlocationy = love.graphics.getHeight() - 100
         love.graphics.setColor(0, 0, 0, 255*70/100)
         love.graphics.rectangle("fill", 0, textboxlocationy, love.graphics.getWidth(), 100)
         love.graphics.setColor(255, 255, 255)
         love.graphics.rectangle("line", 0, textboxlocationy, love.graphics.getWidth(), 100)
-        love.graphics.print(self.text[index], 10, textboxlocationy)
+        love.graphics.print(self.text[self.index], 10, textboxlocationy)
         textarrow:draw(love.graphics.getWidth()-32, textboxlocationy+75)
         if(love.keyboard.isDown("return")) then
             if(self.cooldown==0) then
                 self.index = self.index + 1
-                self.cooldown=20
+                self.cooldown=15
             end
         end
-    --end
+    end
 end
+
+return textbox
--- main.lua
+++ main.lua
@@ -3,18 +3,16 @@ require("lib.AnAL")
 local gamera = require "lib.gamera"
 local map = require("classes.map")
 local p = require("classes.player")
+local b = require("classes.textbox")
 
-local arrow = love.graphics.newImage("Sprites/textarrow.png")
-local textarrow = newAnimation(arrow, 16, 16, 0.1, 1, 2)
-textarrow:setSpeed(0.1)
 
 function love.load()
-  cooldown = 0
   text = {
         "Hello Adventurer", "I used to be an adventurer like you", "until I took an arrow to the knee"
   }
   test = map("Maps.Testmap")
   player=p()
+  box = b(text)
   cam = gamera.new(0,0,512,512)
   cam:setScale(1.5)
   cam:setPosition(player.x, player.y)
@@ -23,9 +21,7 @@ end
 function love.update(dt)
   cam:setPosition(player.x, player.y)
   player:update(dt)
-  if cooldown ~= 0 then
-      cooldown = cooldown-1
-  end
+  box:update(dt)
 end
 
 function love.draw()
@@ -33,19 +29,5 @@ function love.draw()
       test:draw(0,0)
     player:draw()
   end)
-  --if(text[index]) then
-      local textboxlocationy = love.graphics.getHeight() - 100
-      love.graphics.setColor(0, 0, 0, 255*70/100)
-      love.graphics.rectangle("fill", 0, textboxlocationy, love.graphics.getWidth(), 100)
-      love.graphics.setColor(255, 255, 255)
-      love.graphics.rectangle("line", 0, textboxlocationy, love.graphics.getWidth(), 100)
-      love.graphics.print(text[index], 10, textboxlocationy)
-      textarrow:draw(love.graphics.getWidth()-32, textboxlocationy+75)
-      if(love.keyboard.isDown("return")) then
-          if(cooldown==0) then
-              index = index + 1
-              cooldown=20
-          end
-      end
-  --end
+  box:draw()
 end
